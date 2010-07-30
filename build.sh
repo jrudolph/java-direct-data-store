eed42a5b48598bc94e8cdde1bd37e7b06c3cb5ac
Johannes Rudolph <johannes_rudolph@gmx.de>
2010-07-30 09:57:07 +0200

    use colorgcc

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 javac Test.java Person.java &&
 javah Test &&
-gcc -I/usr/lib/jvm/default-java/include \
+colorgcc -I/usr/lib/jvm/default-java/include \
   -I/usr/lib/jvm/default-java/include/linux \
   -lssl \
   -shared -fPIC -o libtest.so -g dump.c reloc.c &&
